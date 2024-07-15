4cd4edeb7c74d9338f83fe2272081d4898394afd
Pedro Faleiros <pedro.alves.faleiros@gmail.com>
2024-07-15 20:52:28 -0300

    feat: add Theme Colors file

---
+++ src/theme/theme.colors.ts
@@ -0,0 +1,18 @@
+const Colors = {
+  primary: "#212529",
+  text: {
+    white: "#F8F9FA",
+    dark: "#343A40",
+  },
+  background: {
+    white: "#F8F9FA",
+    dark: "#212529",
+  },
+  input: {
+    background: "#E9ECEF",
+    placeholder: "#6C757D",
+  },
+  error: "#FFA6A",
+};
+
+export default Colors;
