4765a7f51e1536ce39b260c5eea914b99738c7d9
Pedro Faleiros <pedro.alves.faleiros@gmail.com>
2025-01-13 20:03:29 -0300

    feat: add UserContext

---
+++ src/contexts/user.context.tsx
@@ -0,0 +1,44 @@
+import { createContext, ReactNode, useState } from "react";
+import IUser from "../interfaces/IUser";
+
+interface IUserContext {
+  currentUser: IUser | null;
+  isAuthenticated: boolean;
+  loginUser: (user: IUser) => void;
+  logoutUser: () => void;
+}
+
+export const UserContext = createContext<IUserContext>({
+  currentUser: null,
+  isAuthenticated: false,
+  loginUser: () => {},
+  logoutUser: () => {},
+});
+
+interface UserContextProviderProps {
+  children: ReactNode;
+}
+
+const UserContextProvider = ({ children }: UserContextProviderProps) => {
+  const [currentUser, setCurrentUser] = useState<IUser | null>(null);
+
+  const isAuthenticated = currentUser !== null;
+
+  const loginUser = (user: IUser) => {
+    setCurrentUser(user);
+  };
+
+  const logoutUser = () => {
+    setCurrentUser(null);
+  };
+
+  return (
+    <UserContext.Provider
+      value={{ currentUser, isAuthenticated, loginUser, logoutUser }}
+    >
+      {children}
+    </UserContext.Provider>
+  );
+};
+
+export default UserContextProvider;
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import UserContextProvider from "./contexts/user.context";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <UserContextProvider>
+      <App />
+    </UserContextProvider>
   </React.StrictMode>
 );
 
