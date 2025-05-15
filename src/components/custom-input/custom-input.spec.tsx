cb985f2ae86e667f0dfc592e73b59ff0a492eeee
Pedro Faleiros <pedro.alves.faleiros@gmail.com>
2025-05-14 22:12:54 -0300

    test: ensure custom input change value on user typing

--- src/components/custom-input/custom-input.spec.tsx
+++ src/components/custom-input/custom-input.spec.tsx
@@ -1,6 +1,7 @@
 import { render, screen } from "@testing-library/react";
 import CustomInput from "./custom-input.component";
 import Colors from "../../theme/theme.colors";
+import userEvent from "@testing-library/user-event";
 
 describe("Custom Input", () => {
   it("should render with error if hasError is true", () => {
@@ -17,5 +18,15 @@ describe("Custom Input", () => {
     const input = screen.getByPlaceholderText("Lorem Ipsum");
 
     expect(input).toHaveStyle({ border: "none" });
+  });
+
+  it("should change value when user types", () => {
+    render(<CustomInput placeholder="Lorem Ipsum" hasError={false} />);
+
+    const input = screen.getByPlaceholderText("Lorem Ipsum");
+
+    userEvent.type(input, "Dolor Sit");
+
+    screen.getByDisplayValue("Dolor Sit");
   });
 });
