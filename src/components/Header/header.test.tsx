18f137e5a90bc1e4b398e0096aa66478a110da9e
Pedro Faleiros <pedro.alves.faleiros@gmail.com>
2025-12-13 20:25:44 -0300

    test: ensure cart products count are corectly shown on header

--- src/components/Header/header.test.tsx
+++ src/components/Header/header.test.tsx
@@ -1,6 +1,7 @@
 import { screen } from "@testing-library/react";
 import Header from "./header.component";
 import { renderWithRedux } from "../../helpers/test.helpers";
+import ICartProduct from "../../interfaces/cart";
 
 describe("Header", () => {
   it("should show sign out button when user is authenticated", () => {
@@ -18,5 +19,30 @@ describe("Header", () => {
 
     screen.getByText("Login");
     screen.getByText(/criar conta/i);
+  });
+
+  it("should show correct cart products count", () => {
+    const products: ICartProduct[] = [
+      {
+        id: "1",
+        imageUrl: "image-url",
+        name: "Boné",
+        price: 100,
+        quantity: 2,
+      },
+      {
+        id: "2",
+        imageUrl: "image-url-2",
+        name: "Camiseta",
+        price: 150,
+        quantity: 3,
+      },
+    ];
+
+    renderWithRedux(<Header />, {
+      preloadedState: { cartReducer: { products } },
+    } as any);
+
+    screen.getByText("5");
   });
 });
